a0f5e9319240aec80b3f9c2ffcc8a52279d91cf4
Alasdair <alasdair.nation@gmail.com>
2026-03-16 15:02:59 +0800

    Initial Commit

---
+++ records_manager/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User, Group
+#from django.conf import settings
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    desciption = models.TextField(blank=True)
+    group = models.ForeignKey(Group, models.SET_NULL, blank=True, null=True)
+
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank = True)
+    category = models.ForeignKey(Category, models.SET_NULL, blank=True, null=True)
+    created_date = models.DateField()
+    creator = models.ForeignKey(User,  null=True ,on_delete=models.SET_NULL)
